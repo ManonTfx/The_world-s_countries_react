30e108dea85b37b0f7f9ebc5f23708e2135d7c87
ManonTfx <manon.trefoux@gmail.com>
2021-04-16 15:28:36 +0200

    Add README.md

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Navigation from '../components/Navigation';
+
+const About = () => {
+    return (
+        <div>
+            <Navigation/>
+            <h1>A propos</h1>
+            <br/>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. At repudiandae odit, perferendis nesciunt quos rerum minus consectetur fuga iusto incidunt repellat iste modi cumque veritatis adipisci tempore obcaecati magni expedita dolor excepturi alias non corrupti. Quia consequuntur rem molestiae unde quaerat cupiditate. Delectus pariatur quam porro excepturi nobis. Enim veniam vitae eligendi expedita necessitatibus quasi omnis laudantium possimus unde magni.</p>
+            <br/>
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vitae sed nesciunt eligendi enim quasi impedit, error necessitatibus quas recusandae exercitationem maiores nobis ab facilis, totam debitis numquam! Voluptatibus fugit accusantium ratione laudantium ad ipsum veniam beatae doloribus illo eligendi? Consequatur in perspiciatis tenetur enim repellat minima fugit, rerum perferendis velit aspernatur, rem suscipit repellendus, magni accusamus! Deleniti et, architecto nam nulla quae minima provident dolores molestiae error accusantium? Quod laborum dolore adipisci repudiandae temporibus vitae praesentium distinctio sed. Quia ab accusamus distinctio quod saepe ullam numquam repellat iusto, adipisci error minima nisi hic praesentium, sequi molestiae facere, eius odit atque cum facilis. Quidem corrupti similique adipisci iusto unde rem deserunt provident. Nostrum nulla corporis omnis officiis consequuntur, voluptatem tempora? Reprehenderit reiciendis necessitatibus mollitia dolores. Atque dolorum, explicabo soluta doloribus quibusdam est dolor modi corrupti voluptatibus temporibus beatae cum, recusandae delectus.</p>
+        </div>
+    );
+};
+
+export default About;
